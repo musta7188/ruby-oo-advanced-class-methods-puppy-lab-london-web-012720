57c2769d595c7f59e9f0384bb1839ebe35ac05f7
musta7188 <musta7188@hotmail.it>
2020-02-01 10:39:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require "pry"
+class Dog
+    attr_accessor :name
+
+    @@all = []
+   
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.map {|dogs|puts dogs.name}
+    end
+
+    def self.clear_all
+
+        @@all.clear
+    end
+end
+
